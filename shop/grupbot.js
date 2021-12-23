471ab50b80cf60ed0cb0234fd3529221c54407df
AlphaOfc <https://github.com/AlphaOfc/alpha1.git>
2021-12-23 14:27:14 +0700

    alpha1

---
+++ shop/grupbot.js
@@ -0,0 +1,12 @@
+// jangan di ubah jika tidak punya group bot, takutnya eror
+const gcbotwa = () =>{
+	return`Join Aja Semua Fitur Bot Bisa Digunakan !
+
+1. *Alphabot Support*
+_https://chat.whatsapp.com/BB2gs6PMijN98Ku46tiUAt_
+
+Jika ada link yang ke reset, silahkan hubungi
+owner untuk meminta link yang baru
+`
+}
+exports.gcbotwa = gcbotwa
